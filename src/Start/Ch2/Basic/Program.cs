cba32b39e07cce549ae603bc0b41deccd55b4cf8
Anish Joshi <38525604+anishjoshi1999@users.noreply.github.com>
2024-05-07 03:20:57 +0000

    Handling Multiple Exceptions

--- src/Start/Ch2/Basic/Program.cs
+++ src/Start/Ch2/Basic/Program.cs
@@ -1,15 +1,26 @@
 ﻿// Exercise file for C# Exception and Error Handling by Joe Marini
 // Basic exceptions
+try
+{
+    int[] nums = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-int[] nums = {0,1,2,3,4,5,6,7,8,9};
+    // Dividing by zero causes an exception
+    Console.WriteLine($"Result: {IntDivider(nums[5], nums[1])}");
 
-// Dividing by zero causes an exception
-Console.WriteLine($"Result: {IntDivider(nums[5],nums[2])}");
+    // Indexing an array out of bounds is also an exception
+    Console.WriteLine($"{nums[18]}");
 
-// Indexing an array out of bounds is also an exception
-Console.WriteLine($"{nums[8]}");
-
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine($"{e.Message}");
+}
+catch (IndexOutOfRangeException e)
+{
+    Console.WriteLine($"{e.Message}");
+}
 // A funtion that divides two integers
-int IntDivider(int a, int b) {
+int IntDivider(int a, int b)
+{
     return a / b;
 }
--- src/Start/Ch2/BuiltIn/Program.cs
+++ src/Start/Ch2/BuiltIn/Program.cs
@@ -1,20 +1,23 @@
 ﻿// Exercise file for C# Exception and Error Handling by Joe Marini
 // Built-in exception types
 
-try {
-    int[] nums = {0,1,2,3,4,5,6,7,8,9};
+try
+{
+    int[] nums = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
     // Dividing by zero causes an exception
-    Console.WriteLine($"Result: {IntDivider(nums[4],nums[0])}");
+    Console.WriteLine($"Result: {IntDivider(nums[4], nums[0])}");
 
     // Indexing an array out of bounds is also an exception
     Console.WriteLine($"{nums[15]}");
 }
-catch (Exception e) {
+catch (Exception e)
+{
     Console.WriteLine($"{e.Message}");
 }
 
 // A funtion that divides two integers
-int IntDivider(int a, int b) {
+int IntDivider(int a, int b)
+{
     return a / b;
 }
